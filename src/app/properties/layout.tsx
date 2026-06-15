93ce4b585d1821325a213005a86b907a8821bb2a
rccgregion <rccgregion24@gmail.com>
2026-06-15 14:18:06 +0100

    Delete src directory

--- src/app/properties/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default async function PropertiesLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <main>{children}</main>
-    </div>
-  );
-}
--- src/types/global.d.ts
+++
@@ -1,6 +0,0 @@
-// Allows TypeScript to accept CSS imports as side-effects
-// e.g. import 'leaflet/dist/leaflet.css'
-declare module '*.css' {
-  const content: Record<string, string>;
-  export default content;
-}
